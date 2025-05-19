cfbe389fab0d369bc2fd545d1f686fe2e19bf370
happyworld7017 <aayushdhama@skymute.com>
2025-05-19 23:07:54 +0530

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter as BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import { theme } from "./utils/colors";
 import Card from "./components/card/Card";
