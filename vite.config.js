392d087b29be8f1abb8a71d003eea4d02d0558cb
happyworld7017 <aayushdhama@skymute.com>
2025-05-20 17:08:04 +0530

    domain

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/skymute-website",
+  base: "/skymute-website/",
   plugins: [react(), tailwindcss()],
   server: {
     historyApiFallback: true,
