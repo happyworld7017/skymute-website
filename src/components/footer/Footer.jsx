dce3b6399adf63fd0c654a1431ae458e58aaea50
testingjapan007 <skyaayusumtan7017@gmail.com>
2025-12-03 18:02:47 +0530

    refactor: removed extra imports in footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,3 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { CONSTANTS } from "../../utils/canstants";
-import Android from "../button/Android";
-
 const Footer = () => {
   return (
     <div className="w-full  py-4 border-t border-white ">
