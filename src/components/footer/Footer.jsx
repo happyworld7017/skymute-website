a6d90373298e5c7ab2010dd3e413d59714a9cc8f
testingjapan007 <skyaayusumtan7017@gmail.com>
2025-12-03 17:40:19 +0530

    feat: add address and skymute full name

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,7 +8,13 @@ const Footer = () => {
     <div className="w-full  py-4 border-t border-white ">
       <footer className="max-w-[1100px] mx-auto">
         <ul></ul>
-        <p className="text-xl text-center text-white">&copy; 2025 , skymute</p>
+        <p className="text-xl text-center text-white uppercase">
+          &copy; 2025 skymute technology pvt ltd
+        </p>
+        <p className="text-xl text-center text-slate-400  sm:max-w-[50%] mx-auto pt-4">
+          Registered Address: Organization Address C/o SH Baljor Shekhpur, Pol
+          17/125, Khekra, Baraut Bagpat, Uttar Pradesh 250101 India
+        </p>
       </footer>
     </div>
   );
